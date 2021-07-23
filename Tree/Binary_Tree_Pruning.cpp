e3acc3d84cd765fdf1983d6b9bdeb9291601b1b2
hemangdtu <hemang.dtu@gmail.com>
2021-07-23 17:03:48 +0530

    Updating files

---
+++ Tree/Binary_Tree_Pruning.cpp
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* pruneTree(TreeNode* root) {
+        if(root == NULL)
+            return root;
+        
+        TreeNode* left = pruneTree(root -> left);
+        root -> left = left;
+        TreeNode* right = pruneTree(root -> right);
+        root -> right = right;
+        
+        if(root -> val == 0 && root -> left == NULL && root -> right == NULL)
+            root = NULL;
+        return root;
+    }
+};
