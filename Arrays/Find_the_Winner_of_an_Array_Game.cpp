142cecd11b1182e1a4a29c8fcfb93912b4c13182
hemangdtu <hemang.dtu@gmail.com>
2021-07-24 23:38:52 +0530

    Updating files

---
+++ Arrays/Find_the_Winner_of_an_Array_Game.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int getWinner(vector<int>& arr, int k) {
+        int result = arr[0];
+        for (int i = 1, count = 0; i < arr.size(); i++)
+        {
+            if(arr[i] > result)
+            {
+                result = arr[i];
+                count = 0;
+            }
+            if(++count == k)
+                break;
+        }
+        return result;
+    }
+};
