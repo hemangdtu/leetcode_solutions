544a3c52ea42cddc53f20e125fdf8c5248ed46a4
hemangdtu <hemang.dtu@gmail.com>
2021-08-05 23:36:25 +0530

    Updating files

---
+++ Arrays/Stone_Game.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool stoneGame(vector<int>& piles) {
+        if (piles.size() % 2 == 0 || piles.size() == 1)
+            return true;
+        
+        vector<int> dp(piles.size());
+        for(int i = piles.size() - 1; i >= 0; --i)
+        {
+            dp[i] = piles[i];
+            for(int j = i + 1; j < piles.size(); ++j)
+                dp[j] = max(piles[i] - dp[j], piles[j] - dp[j - 1]);
+        }
+        return dp.back() >= 0;
+    }
+};
